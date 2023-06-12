9aa3ef5f65d432669ea99d5a77a049254da56475
colidom <colidom@outlook.com>
2023-06-12 19:25:04 +0100

    Añadida primera funcion

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import telebot
 
 api = "6225579104:AAHyGu3zfApAUT68SyhOcOoeL8zV6SerHMQ"
-
 bender = telebot.TeleBot(api)
+
+@bender.message_handler(commands = ["help", "start"])
+def send_message(message):
+    bender.reply_to(message, "Hi there!")
+
+bender.polling()
